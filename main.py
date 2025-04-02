427f6ea99ffb4050fd445eba6065963f97bec1c8
Nikita Tselischev <tselishchev.na@phystech.edu>
2025-04-02 12:36:45 +0300

    Homework 1 done

---
+++ main.py
@@ -0,0 +1,27 @@
+from matrix import Matrix
+from tensor import Tensor
+
+if __name__ == "__main__":
+    t = Tensor(3, [1, 2, 3])
+    m = Matrix(10, 10, [i for i in range(100)])
+    print(m)
+    print(m[1, 1])
+    print(m[1])
+    print(m[-1])
+    print(m[1:4])
+    print(m[:4])
+    print(m[4:])
+    print(m[:])
+    print(m[1:7:2])
+    print(m[:, 1])
+    print(m[1:4, 1:4])
+    print(m[1:4, :4])
+    print(m[1:4, 4:])
+    print(m[1:4, :])
+    print(m[-1:])
+    print(m[-2::-2])
+    print(m[-2::-2, 1:4])
+    print(m[:, :])
+    print(m[[1, 4]])
+    print(m[:, [1, 4]])
+    print(m[[1, 4], [1, 4]])
